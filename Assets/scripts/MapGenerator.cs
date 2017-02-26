6eed58c04ecc197f561f9c854695ddfb253ef691
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-02-26 20:42:46 +0000

    Add level of detail slider to generated mesh

--- Assets/scripts/MapGenerator.cs
+++ Assets/scripts/MapGenerator.cs
@@ -2,6 +2,8 @@
 
 public class MapGenerator : MonoBehaviour {
 
+    private const int MAP_CHUNK_SIZE = 241;
+
     public enum DrawMode {
         Noise,
         ColorMap,
@@ -10,8 +12,6 @@ public class MapGenerator : MonoBehaviour {
 
     public DrawMode drawMode;
 
-	public int mapWidth;
-    public int mapHeight;
     public float noiseScale;
 
     public int octaves;
@@ -25,19 +25,22 @@ public class MapGenerator : MonoBehaviour {
     public float meshHeightMultiplier;
     public AnimationCurve meshHeightCurve;
 
+    [RangeAttribute (0, 6)]
+    public int levelOfDetail;
+
     public bool autoUpdate;
 
     public TerrainType [] regions;
 
     public void GenerateMap ()
     {
-        float [,] noiseMap = Noise.GenerateNoiseMap (mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+        float [,] noiseMap = Noise.GenerateNoiseMap (MAP_CHUNK_SIZE, MAP_CHUNK_SIZE, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
-        Color [] colorMap = new Color [mapWidth * mapHeight];
+        Color [] colorMap = new Color [MAP_CHUNK_SIZE * MAP_CHUNK_SIZE];
 
-        for (int x = 0; x < mapWidth; x++)
+        for (int x = 0; x < MAP_CHUNK_SIZE; x++)
         {
-            for (int y = 0; y < mapHeight; y++)
+            for (int y = 0; y < MAP_CHUNK_SIZE; y++)
             {
                 float currentHeight = noiseMap [x, y];
                 
@@ -45,7 +48,7 @@ public class MapGenerator : MonoBehaviour {
                 {
                     if (currentHeight <= regions [i].height)
                     {
-                        colorMap [y * mapWidth + x] = regions [i].color;
+                        colorMap [y * MAP_CHUNK_SIZE + x] = regions [i].color;
                         break;
                     }
                 }
@@ -59,24 +62,16 @@ public class MapGenerator : MonoBehaviour {
         }
         else if (drawMode == DrawMode.ColorMap)
         {
-            display.DrawTexture (TextureGenerator.TextureFromColorMap (colorMap, mapWidth, mapHeight));
+            display.DrawTexture (TextureGenerator.TextureFromColorMap (colorMap, MAP_CHUNK_SIZE, MAP_CHUNK_SIZE));
         }
         else if (drawMode == DrawMode.Mesh)
         {
-            display.DrawMesh (MeshGenerator.GenerateTerrainMesh (noiseMap, meshHeightMultiplier, meshHeightCurve), TextureGenerator.TextureFromColorMap (colorMap, mapWidth, mapHeight));
+            display.DrawMesh (MeshGenerator.GenerateTerrainMesh (noiseMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail), TextureGenerator.TextureFromColorMap (colorMap, MAP_CHUNK_SIZE, MAP_CHUNK_SIZE));
         }
     }
 
     private void OnValidate ()
     {
-        if (mapWidth < 1)
-        {
-            mapWidth = 1;
-        }
-        if (mapHeight < 1)
-        {
-            mapHeight = 1;
-        }
         if (octaves < 0)
         {
             octaves = 0;
--- Assets/scripts/MeshGenerator.cs
+++ Assets/scripts/MeshGenerator.cs
@@ -2,7 +2,7 @@
 
 public static class MeshGenerator {
 
-    public static MeshData GenerateTerrainMesh (float[,] heightMap, float heightMultiplier, AnimationCurve heightCurve)
+    public static MeshData GenerateTerrainMesh (float[,] heightMap, float heightMultiplier, AnimationCurve heightCurve, int levelOfDetail)
     {
         int width = heightMap.GetLength (0);
         int height = heightMap.GetLength (1);
@@ -10,20 +10,23 @@ public static class MeshGenerator {
         float topLeftX = (width - 1) / -2f;
         float topLeftZ = (height - 1) / 2f;
 
-        MeshData meshData = new MeshData (width, height);
+        int meshSimplificationIncrement = (levelOfDetail == 0) ? 1 : levelOfDetail * 2;
+        int verticesPerLine = ((width - 1) / meshSimplificationIncrement) + 1;
+
+        MeshData meshData = new MeshData (verticesPerLine, verticesPerLine);
         int vertexIndex = 0;
 
-        for (int x = 0; x < width; x++)
+        for (int x = 0; x < width; x += meshSimplificationIncrement)
         {
-            for (int y = 0; y < height; y++)
+            for (int y = 0; y < height; y += meshSimplificationIncrement)
             {
                 meshData.vertices [vertexIndex] = new Vector3 (topLeftX + x, heightCurve.Evaluate (heightMap [x, y]) * heightMultiplier, topLeftZ - y);
                 meshData.uvs [vertexIndex] = new Vector2 (x / (float) width, y / (float) height);
 
                 if (x < width - 1 && y < height - 1)
                 {
-                    meshData.AddTriangle (vertexIndex + width, vertexIndex + width + 1, vertexIndex);
-                    meshData.AddTriangle (vertexIndex + 1, vertexIndex, vertexIndex + width + 1);
+                    meshData.AddTriangle (vertexIndex + verticesPerLine, vertexIndex + verticesPerLine + 1, vertexIndex);
+                    meshData.AddTriangle (vertexIndex + 1, vertexIndex, vertexIndex + verticesPerLine + 1);
                 }
 
                 vertexIndex++;
